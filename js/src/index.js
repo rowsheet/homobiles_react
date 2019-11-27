fc0db8fa89520ecda069b721095c5b366f4f3572
Peter Harrison <peterharrisonjr@gmail.com>
2019-11-27 02:08:54 -0700

    Adding basic router.

--- js/src/index.js
+++ js/src/index.js
@@ -1,11 +1,55 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function Foo() {
+  return <h2>Foo</h2>;
+}
+
+function Bar() {
+  return <h2>Bar</h2>;
+}
+
+function Baz() {
+  return <h2>Baz</h2>;
+}
+
+var component_routes = {
+    "/foo": Foo,
+    "/bar": Bar,
+    "/baz": Baz,
+    "/": Home,
+}
+
 class App extends React.Component {
-     render() {
-          return (
-               <div>
-                    This is: {this.props.name}
-               </div>
-          );
-     }
+    render() {
+        var nav_links = Object.keys(component_routes).map(route => (
+            <li><Link to={ route }>{ route }</Link></li>));
+        var routes = Object.keys(component_routes).map(route => (
+            <Route path={ route }>{ React.createElement(component_routes[route]) }</Route>));
+        return (
+<Router>
+    <div>
+        <nav>
+            <ul>
+                { nav_links.map( link => link) }
+            </ul>
+        </nav>
+        <Switch>
+            { routes.map( route => route ) }
+        </Switch>
+    </div>
+</Router>
+        );
+    }
 }
 
 ReactDOM.render(
